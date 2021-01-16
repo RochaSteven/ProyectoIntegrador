7a1122603e5a91249b1a3e8ea83260985c0d3ff5
RochaSteven <sat.rocha@yavirac.edu.ec>
2021-01-16 16:10:53 -0500

    hola1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{ 
+  
+    public float velocidad=2;
+    public GameObject col;
+    public GameObject piedra;
+    public GameObject piedra2;
+    public GameObject menuPrincipal;
+    public GameObject menuGameOver;
+    public bool gameOver=false;
+    public bool start=false;
+    public Renderer fondo;
+    public List<GameObject> cols;
+    public List<GameObject> obstaculos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //creare mapa
+        for(int i=0; i<21; i++)
+        {
+          cols.Add(Instantiate(col, new Vector2(-10 + i,-3), Quaternion.identity));
+        }
+
+        //crear obstaculos
+        obstaculos.Add(Instantiate(piedra, new Vector2(14,-2),Quaternion.identity ));
+        obstaculos.Add(Instantiate(piedra2, new Vector2(18,-2),Quaternion.identity ));
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+    {
+      if(start==false)
+      {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            start= true;
+        }
+      }
+    
+       if(start==true&&gameOver==true)
+      {
+        menuGameOver.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+      }
+
+
+      if (start==true && gameOver==false)
+      {
+        menuPrincipal.SetActive(false);
+          fondo.material.mainTextureOffset=fondo.material.mainTextureOffset + new Vector2(0.025f, 0) * Time.deltaTime;  
+      //mover mapa
+      for(int i=0; i<cols.Count; i++)
+        {if (cols[i].transform.position.x <= -10)
+        {
+            cols[i].transform.position=new Vector3(10,-3, 0 );
+        }
+          cols[i].transform.position=cols[i].transform.position + new Vector3(-1,0,0)*Time.deltaTime* velocidad;
+        }
+
+        //mover obstaculos
+         for(int i=0; i<obstaculos.Count; i++)
+        {if (obstaculos[i].transform.position.x <= -10)
+        {
+          float randomObs= Random.Range(11,18);
+            obstaculos[i].transform.position = new Vector3 (randomObs,-2,0);
+        }
+          obstaculos[i].transform.position=obstaculos[i].transform.position + new Vector3(-1,0,0)*Time.deltaTime* velocidad;
+        }
+      }
+
+    }
+}
